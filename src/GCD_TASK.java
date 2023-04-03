ad295041f185f76efbef4f43974e1d977fb47a06
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-03 21:15:41 +0600

    Finding GCD program

--- src/GCD_TASK.java
+++ src/GCD_TASK.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 //Karpovich A.D. SE-2218 220174
 public class GCD_TASK {
     //a recursive function for finding GCD of two numbers...
-    public static int GCD(int a, int b) {
+    public static int GCD10TASK(int a, int b) {
         //important condition
         if (b == 0) {
             return a;
         } else {
-            return GCD(b, a % b); //a recursive algorithm for finding GCD (Euclidean Algorithm)
+            return GCD10TASK(b, a % b); //a recursive algorithm for finding GCD (Euclidean Algorithm)
         }
     }
 
@@ -16,7 +16,7 @@ public class GCD_TASK {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); //getting values from user
         int k = sc.nextInt();
-        System.out.println(GCD(n, k)); //using our recursive function with parameters by user
+        System.out.println(GCD10TASK(n, k)); //using our recursive function with parameters by user
     }
 
 }
