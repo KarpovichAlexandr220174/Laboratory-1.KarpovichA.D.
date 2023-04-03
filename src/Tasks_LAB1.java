315396d5795a4032187ceb3b21929a09c8f653aa
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-03 17:17:02 +0600

    Initial commit

---
+++ src/Tasks_LAB1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Tasks_LAB1 {
+    //a recursive function for finding Minimum in array...
+    public static int findMininum(int[] arr, int n) {
+        if (n == 1) {
+            return arr[0];
+        } else {
+            return Math.min(findMininum(arr, n - 1), arr[n - 1]); //main recursive algorithm
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt(); //array filling by user
+        }
+        System.out.println(findMininum(arr, n)); //using our recursive function with parameters by user
+    }
+}
