01d13a35b9ce146ce3712a20fc1e02d26a9f69ac
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-03 17:41:59 +0600

    Rename a method

--- src/findMinimum.java
+++ src/findMinimum.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class findMinimum {
     //a recursive function for finding Minimum in array...
-    public static int findMininum(int[] arr, int n) {
+    public static int findMininum1TASK(int[] arr, int n) {
         if (n == 1) {
             return arr[0];
         } else {
-            return Math.min(findMininum(arr, n - 1), arr[n - 1]); //main recursive algorithm
+            return Math.min(findMininum1TASK(arr, n - 1), arr[n - 1]); //main recursive algorithm
         }
     }
 
@@ -17,6 +17,6 @@ public class findMinimum {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt(); //array filling by user
         }
-        System.out.println(findMininum(arr, n)); //using our recursive function with parameters by user
+        System.out.println(findMininum1TASK(arr, n)); //using our recursive function with parameters by user
     }
 }
