90d1d4b3dc738be5134fd7292805377e5b181a92
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-09 19:12:43 +0600

    Finding Fibonacci program

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 //Karpovich A.D. SE-2218 220174
 public class Fibonacci {
     //a recursive function for finding Fibonacci
-    public static int Fibonacci5TASK(int n) {
+    public static int getFibonacci5TASK(int n) {
         //main conditions
         if (n == 0) {
             return 0;
         } else if (n == 1) {
             return 1;
         } else {
-            return Fibonacci5TASK(n - 1) + Fibonacci5TASK(n - 2); //a recursive algorithm of Fibonacci
+            return getFibonacci5TASK(n - 1) + getFibonacci5TASK(n - 2); //a recursive algorithm of Fibonacci
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); //getting a value from user
-        System.out.println(Fibonacci5TASK(n)); //using our recursive function with parameters by user
+        System.out.println(getFibonacci5TASK(n)); //using our recursive function with parameters by user
     }
 
 }
