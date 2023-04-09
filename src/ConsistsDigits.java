ed8ad250f8ad5a7f54055bcc1fcbff991f2e2711
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-09 19:19:06 +0600

    Checking for "all digits" program

--- src/ConsistsDigits.java
+++ src/ConsistsDigits.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 //Karpovich A.D. SE-2218 220174
 public class ConsistsDigits {
     //a recursive function for checking for "all digits"...
-    public static Boolean StringChecking8TASK(String s) {
+    public static Boolean checkString8TASK(String s) {
         //main conditions
         if (s.isEmpty()) {
             return true; //if the string is empty
@@ -12,14 +12,14 @@ public class ConsistsDigits {
         if (!Character.isDigit(element)) {
             return false; //if the first element is not a digit, with a symbol '!' in condition code, that means "not"
         }
-        return StringChecking8TASK(s.substring(1)); //a recursive algorithm with
+        return checkString8TASK(s.substring(1)); //a recursive algorithm with
         //"substring" for cutting the string and checking for digits
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String stroka = sc.next(); //getting a string from user
-        System.out.println(StringChecking8TASK(stroka)); //using our recursive function with parameters by user
+        System.out.println(checkString8TASK(stroka)); //using our recursive function with parameters by user
     }
 
 
