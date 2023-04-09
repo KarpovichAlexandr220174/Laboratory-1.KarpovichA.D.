6f064db51d4fd28e1a83e6a4991bb3c4461fce75
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-09 21:45:57 +0600

    Finding an average program

--- src/findAverage.java
+++ src/findAverage.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
+
 //Karpovich A.D. SE-2218 220174
+/*
+    @  findAverage2TASK - definition of average
+    @ n - length of array
+    @ arr - array
+    @ return - returning the average of array
+*/
 public class findAverage {
     //a recursive function for finding average of array...
     public static int findAverage2TASK(int[] arr, int n) {
