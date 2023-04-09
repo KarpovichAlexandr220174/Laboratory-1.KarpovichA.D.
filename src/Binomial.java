59e1c77632ac30ce00112b1d68f0cd1e4d21eb82
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-09 19:21:46 +0600

    Finding Binomial program

--- src/Binomial.java
+++ src/Binomial.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 //Karpovich A.D. SE-2218 220174
 public class Binomial {
     //a recursive function for finding Binomial Coeff...
-    public static int binomialCoefficient9TASK(int n, int k) {
+    public static int findBinomial9TASK(int n, int k) {
         //important condition from task
         if (k == 0 || k == n) {
             return 1;
         }
-        return binomialCoefficient9TASK(n - 1, k - 1) + binomialCoefficient9TASK(n - 1, k); //a recursive
+        return findBinomial9TASK(n - 1, k - 1) + findBinomial9TASK(n - 1, k); //a recursive
         //algorithm for finding binomial coefficient
     }
 
@@ -16,7 +16,7 @@ public class Binomial {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); //getting values from user
         int k = sc.nextInt();
-        System.out.println(binomialCoefficient9TASK(n, k)); //using our recursive function with parameters by user
+        System.out.println(findBinomial9TASK(n, k)); //using our recursive function with parameters by user
     }
 
 }
